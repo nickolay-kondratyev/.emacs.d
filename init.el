a13d6ac4d42b56e289ea6de65b2f0b2593012aa5
Nickolay Kondratyev <nickolay.kondratyev@gmail.com>
2013-10-13 21:56:34 -0700

    Adding auto-complete

--- init.el
+++ init.el
@@ -1,1 +1,10 @@
 (load-theme 'deeper-blue)
+
+;;;;;;;;auto-complete;;;;;;;;
+;;Enable default auto complete
+(add-to-list 'load-path "~/.emacs.d/plugins/auto_complete/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto_complete//ac-dict")
+(ac-config-default)
+;;;;;;;;auto-complete end;;;;;;;;
+
