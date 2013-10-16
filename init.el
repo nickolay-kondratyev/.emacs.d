ebbaeefb960b4f7d1f3e2e9dcfdfc555fa4c39d7
Nickolay Kondratyev <nickolay.kondratyev@gmail.com>
2013-10-15 22:22:38 -0700

    Adding yasnippet

--- init.el
+++ init.el
@@ -190,4 +190,11 @@
 (require 'xcscope)
 ;;;;;;;;CScope end;;;;;;;;
 
+;;;;;;;;yasnippet;;;;;;;;
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
+;;;;;;;;yasnippet end;;;;;;;;
+
 
